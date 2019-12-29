34e979eda87b355b87b46a679df70516618b5f45
kalsbold <kalsbold@nate.com>
2019-12-29 17:16:10 +0900

    코멘트 좀 달았음.

---
+++ cplusplus_tuto/Variable.cpp
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//변수 : 어떤 값을 저장할 자리.
+
+	//논리형
+	bool tf;
+
+	//정수형
+	char a;
+	short b;
+	int c;
+	long d;
+	long long e;
+
+	//실수형.
+	float f;
+	double g;
+	long double h;
+
+	//초기화
+
+	//형변환
+
+	//기본 입출력
+
+	//이스케이프 시퀀스
+
+	//형식지정자
+
+	//실수형 소수점.
+
+	//상수
+}
+
+
--- cplusplus_tuto/cplusplus_tuto.cpp
+++ cplusplus_tuto/cplusplus_tuto.cpp
@@ -9,4 +9,19 @@ int main()
     std::cout << "Hello World!\n"; 
 }
 
+////C   참고  https://wikidocs.net/book/314   https://dojang.io/course/view.php?id=2
+////C++ 참고  https://wikidocs.net/21186
 
+
+/*
+
+static 
+
+extern : 외부 소스 파일 전역 변수 참조 키워드.
+
+auto //c와 c++에서 사용법과 기능이 완벽하게 다르므로 제외.  c++만 기억하면 됨.
+
+register : 레지스터에 직접 저장되는 변수 키워드. 지역변수에만 사용가능. 개수제한 있음. 나중에 굳이 필요하면 쓰는데 이미 최적화단계가 건드릴 필요조차 없을정도. 임베디드 아니면 무시해도 되는 키워드.
+
+
+*/
